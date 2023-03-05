fd49c7bd7feac2cea6624217b0ad053af0df40a2
RillSvai <kirilkvas07@gmail.com>
2023-03-05 20:53:36 +0200

    Probably end2

--- hw1/Program.cs
+++ hw1/Program.cs
@@ -3,7 +3,12 @@
 SquareMatrix matrix = new SquareMatrix(@"C:\Kiril\C#\num-methods\hw1\hw1\matrix.txt", 4);
 Console.WriteLine($"=====Start matrix=====\n{matrix.ToString()}\n\n=====Transformed matrix=====\n");
 double _determinate = matrix.ToTriangularForm();
-Console.WriteLine(_determinate == 0 ? "Matrix is degenerate" : $"Determinate:\n{_determinate}\n");
+if (_determinate == 0) 
+{
+    Console.WriteLine("Matrix is degenerated!");
+    return;
+} 
+Console.WriteLine($"Determinate:\n{_determinate}\n");
 matrix.FindJunctions();
 Console.WriteLine(matrix.ToString());
 matrix.CheckResult();
